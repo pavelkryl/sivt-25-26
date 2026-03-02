993ad0d4a7b89f7e52146b48f8b43e9803851eae
Pavel Kryl <pavel@kryl.eu>
2026-03-02 15:42:31 +0100

    limit the torch download to CPU version

--- lekce-21-ann/prepare.sh
+++ lekce-21-ann/prepare.sh
@@ -2,4 +2,7 @@
 . .venv/bin/activate
 
 pip install --upgrade pip
+
+pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
+
 pip install pillow huggingface_hub ultralytics
