05a0f074458a3d8f56fec2c7436ce7ed96e375f4
Pavel Kryl <pavel@kryl.eu>
2026-03-16 15:50:20 +0100

    Implement Calculator class methods and initialize stack

--- lekce-22-objektove-programovani/calculator_stateful.py
+++ lekce-22-objektove-programovani/calculator_stateful.py
@@ -1,11 +1,30 @@
 
 class Calculator:
 
-    def set_op(self, op: float):
-        ...
+    def __init__(self):
+        self._zasobnik : list[float] = []
 
-    def minus(self):
-        ...
 
-    def plus(self):
-        ...
+    def set_op(self, op: float) -> None:
+        self._zasobnik.append(op)
+
+    def krat(self) -> float:
+        op1 = self._zasobnik.pop()
+        op2 = self._zasobnik.pop()
+        return op1 * op2
+
+    def plus(self) -> float:
+        op1 = self._zasobnik.pop()
+        op2 = self._zasobnik.pop()
+        return op1 + op2
+
+
+#
+c = Calculator()
+c.set_op(2)
+c.set_op(1)
+c.set_op(3)
+vysledek = c.plus()
+c.set_op(vysledek)
+vysledek = c.krat()
+print(vysledek)
