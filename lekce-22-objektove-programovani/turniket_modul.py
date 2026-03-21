854f587d9b250ab35f92e7d4e9db2d45d4c9b142
Pavel Kryl <pavel@kryl.eu>
2026-03-21 11:12:33 +0100

    objektovy turniket

--- lekce-22-objektove-programovani/turniket_modul.py
+++ lekce-22-objektove-programovani/turniket_modul.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+from typing import Optional
+
 from pydantic import BaseModel
 
 
@@ -9,27 +11,27 @@ class Pristup(BaseModel):
 
 
 class Turniket:
-    nazev: str
-    platne_skipasy : list[str] = []
-    log_pristupu : list[Pristup] = []
 
+    def __init__(self, nazev: str, platne_skipasy : Optional[list[str]]) -> None:
+        self._nazev: str = nazev
+        self._platne_skipasy : list[str] = platne_skipasy if platne_skipasy is not None else []
+        self._log_pristupu : list[Pristup] = []
 
-# API
-def vpustit(turniket: Turniket, rfid: str) -> bool:
-    r = rfid in turniket.platne_skipasy
-    turniket.log_pristupu.append(Pristup(rfid=rfid, cas_pristupu=datetime.now(), povoleno=r))
-    return r
+    def vpustit(self, rfid: str) -> bool:
+        r = rfid in self._platne_skipasy
+        self._log_pristupu.append(Pristup(rfid=rfid, cas_pristupu=datetime.now(), povoleno=r))
+        return r
 
-def reset(turniket: Turniket) -> None:
-    turniket.platne_skipasy = []
-    turniket.log_pristupu = []
+    def reset(self) -> None:
+        self._platne_skipasy = []
+        self._log_pristupu = []
 
-def iniciace(turniket: Turniket, skipasy: list[str]) -> None:
-    turniket.platne_skipasy = skipasy
+    def iniciace(self, skipasy: list[str]) -> None:
+        self._platne_skipasy = skipasy
 
-def pridej(turniket: Turniket, zakoupeny_skipas_rfid: str) -> None:
-    turniket.platne_skipasy.append(zakoupeny_skipas_rfid)
+    def pridej_skipas(self, zakoupeny_skipas_rfid: str) -> None:
+        self._platne_skipasy.append(zakoupeny_skipas_rfid)
 
-# tohle je se stava nadbytecne, ale nechavam to zde
-def log_vstupu(turniket: Turniket) -> list[Pristup]:
-    return turniket.log_pristupu
+    # tohle je se stava nadbytecne, ale nechavam to zde
+    def log_vstupu(self) -> list[Pristup]:
+        return self._log_pristupu
